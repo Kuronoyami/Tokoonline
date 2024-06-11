451224b22491639af2e0a9fd8b478312b312df53
unknown <hatzgamegi04@gmail.com>
2024-06-11 17:05:24 +0700

    revisi 2

--- tokokita_1234/application/controllers/adminpanel.php
+++ tokokita_1234/application/controllers/adminpanel.php
@@ -5,4 +5,11 @@ class Adminpanel extends CI_Controller {
     public function index(){
         $this->load->view('admin/login');
     }
+
+    public function dashboard(){
+        $this->load->view('admin/layout/header');
+        $this->load->view('admin/layout/menu');
+        $this->load->view('admin/layout/footer');
+        $this->load->view('admin/dashboard');
+    }
 }
